705d250c42f8c124e9ed45b66d098e9f70391c9e
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2021-09-25 14:36:28 +0530

    feat: shows meaning upon entering emoji

--- src/App.js
+++ src/App.js
@@ -2,11 +2,34 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
+  var emojidic = {
+    "🙂": "smile",
+    hey: "there",
+  };
+
+  var emojis = Object.keys(emojidic);
+
+  const [meaning, setMeaning] = useState("");
+
+  function eventHandler(event) {
+    var emojiInput = event.target.value;
+    var meaning = emojidic[emojiInput];
+
+    if (meaning === undefined) {
+      meaning = "we do not have the emoji in our database";
+    }
+    setMeaning(meaning);
+  }
+
   return (
     <div className="App">
       <header>
         <h1 className="logo">EmojiMeans</h1>
       </header>
+
+      <input className="input" onChange={eventHandler} />
+      {/* <button onClick={}>click me</button> */}
+      <div className="meaning">{meaning}</div>
     </div>
   );
 }
